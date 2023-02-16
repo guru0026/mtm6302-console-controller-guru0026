7e59579659f44ff08eecd8a9aaabffd2b2e12150
Hari Gurusubramanian <guru0026@algonquinlive.com>
2023-02-15 23:07:49 -0500

    adds header, body, background and color functions

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-function setTitle () {
+// SETS HEADER TEXT TO INPUT CONTENT========================
+const $headerText = document.getElementById('headerText');
 
+function setTitle ( $newTitle ) {
+    $headerText.textContent = $newTitle;
 }
 
-function setDescription () {
 
+// SETS BODY TEXT TO INPUT CONTENT==========================
+const $bodyText = document.getElementById('bodyText');
+
+function setDescription ( $newBody ) {
+    $bodyText.textContent = $newBody;
 }
 
-function setBackgroundColor () {
+// SETS BACKGROUND COLOUR TO INPUT NAME=====================
+const $bodyStyle = document.body.style;
 
+function setBackgroundColor ( $newBG ) {
+    $bodyStyle.backgroundColor = $newBG; 
 }
 
-function setFontColor () {
-
+// SET FONT COLOUR TO INPUT NAME============================
+function setFontColor ( $newColor) {
+    $bodyStyle.color = $newColor;
 }
 
 function setTheme () {
